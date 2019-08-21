3210690c8508f3330d0fcc4037b2f40eaaa23661
Ryan L Hayes <rhaye@umich.edu>
2019-08-21 14:58:31 -0400

    Have a faster version of the cull kernel, but it crashes with multiple nodes. Changed code indent of working cull kernel to match, so I can easily add in features to see what breaks it.

--- src/domdec/cull.cu
+++ src/domdec/cull.cu
@@ -71,30 +71,30 @@ __global__ void cull_blocks_kernel(int3 idDomdec,int3 gridDomdec,int *blockCount
 // For the small number of domains here, the naive approach below might be more efficient.
 
   if (iBlock<blockCount[domainIdx+1]) {
-    // (0,0,0) interacts with (1,x,x), (0,1,x), (0,0,1), and (0,0,0), x={-1,0,1}
-    partnerPos=0;
-    startBlock=i/32; // For first (0,0,0) domain only
-    for (idShift.x=0; idShift.x<2; idShift.x++) {
-      idPartnerDomain.x=idDomdec.x+idShift.x;
-      s=(idPartnerDomain.x==gridDomdec.x?1:0);
-      s=(idPartnerDomain.x==-1?-1:s);
-      idPartnerDomain.x-=s*gridDomdec.x;
-      shift.x=s;
-      boxShift.x=s*box.x;
-      for (idShift.y=-idShift.x; idShift.y<2; idShift.y++) {
-        idPartnerDomain.y=idDomdec.y+idShift.y;
-        s=(idPartnerDomain.y==gridDomdec.y?1:0);
-        s=(idPartnerDomain.y==-1?-1:s);
-        idPartnerDomain.y-=s*gridDomdec.y;
-        shift.y=s;
-        boxShift.y=s*box.y;
-        for (idShift.z=-((idShift.x!=0)|(idShift.y!=0)); idShift.z<2; idShift.z++) {
-          idPartnerDomain.z=idDomdec.z+idShift.z;
-          s=(idPartnerDomain.z==gridDomdec.z?1:0);
-          s=(idPartnerDomain.z==-1?-1:s);
-          idPartnerDomain.z-=s*gridDomdec.z;
-          shift.z=s;
-          boxShift.z=s*box.z;
+  // (0,0,0) interacts with (1,x,x), (0,1,x), (0,0,1), and (0,0,0), x={-1,0,1}
+  partnerPos=0;
+  startBlock=i/32; // For first (0,0,0) domain only
+  for (idShift.x=0; idShift.x<2; idShift.x++) {
+    idPartnerDomain.x=idDomdec.x+idShift.x;
+    s=(idPartnerDomain.x==gridDomdec.x?1:0);
+    s=(idPartnerDomain.x==-1?-1:s);
+    idPartnerDomain.x-=s*gridDomdec.x;
+    shift.x=s;
+    boxShift.x=s*box.x;
+    for (idShift.y=-idShift.x; idShift.y<2; idShift.y++) {
+      idPartnerDomain.y=idDomdec.y+idShift.y;
+      s=(idPartnerDomain.y==gridDomdec.y?1:0);
+      s=(idPartnerDomain.y==-1?-1:s);
+      idPartnerDomain.y-=s*gridDomdec.y;
+      shift.y=s;
+      boxShift.y=s*box.y;
+      for (idShift.z=-((idShift.x!=0)|(idShift.y!=0)); idShift.z<2; idShift.z++) {
+        idPartnerDomain.z=idDomdec.z+idShift.z;
+        s=(idPartnerDomain.z==gridDomdec.z?1:0);
+        s=(idPartnerDomain.z==-1?-1:s);
+        idPartnerDomain.z-=s*gridDomdec.z;
+        shift.z=s;
+        boxShift.z=s*box.z;
 
           partnerDomainIdx=(idPartnerDomain.x*gridDomdec.y+idPartnerDomain.y)*gridDomdec.z+idPartnerDomain.z;
           startBlock+=blockCount[partnerDomainIdx];
@@ -105,44 +105,44 @@ __global__ void cull_blocks_kernel(int3 idDomdec,int3 gridDomdec,int *blockCount
 
           // Check potential partner blocks in groups of 32.
           for (j=startBlock; j<endBlock; j+=32) {
-            // Check if this block is interacting
-            hit=false;
-            if (j+(i&31)<endBlock) {
-              partnerVolume=blockVolume[j+(i&31)];
-              hit=check_proximity(volume,partnerVolume,rc2);
-            }
+              // Check if this block is interacting
+              hit=false;
+              if (j+(i&31)<endBlock) {
+                partnerVolume=blockVolume[j+(i&31)];
+                hit=check_proximity(volume,partnerVolume,rc2);
+              }
 
-            // see how many hits partner threads got
-            __syncwarp();
-            cumHit=hit;
-            passHit=((i&1)?0:cumHit); // (i&1) receive
-            cumHit+=__shfl_sync(0xFFFFFFFF,passHit,(i|0)-1);
-            passHit=((i&2)?0:cumHit); // (i&2) receive
-            cumHit+=__shfl_sync(0xFFFFFFFF,passHit,(i|1)-2);
-            passHit=((i&4)?0:cumHit); // (i&4) receive
-            cumHit+=__shfl_sync(0xFFFFFFFF,passHit,(i|3)-4);
-            passHit=((i&8)?0:cumHit); // (i&8) receive
-            cumHit+=__shfl_sync(0xFFFFFFFF,passHit,(i|7)-8);
-            passHit=((i&16)?0:cumHit); // (i&16) receive
-            cumHit+=__shfl_sync(0xFFFFFFFF,passHit,(i|15)-16);
+                // see how many hits partner threads got
+                __syncwarp();
+                cumHit=hit;
+                passHit=((i&1)?0:cumHit); // (i&1) receive
+                cumHit+=__shfl_sync(0xFFFFFFFF,passHit,(i|0)-1);
+                passHit=((i&2)?0:cumHit); // (i&2) receive
+                cumHit+=__shfl_sync(0xFFFFFFFF,passHit,(i|1)-2);
+                passHit=((i&4)?0:cumHit); // (i&4) receive
+                cumHit+=__shfl_sync(0xFFFFFFFF,passHit,(i|3)-4);
+                passHit=((i&8)?0:cumHit); // (i&8) receive
+                cumHit+=__shfl_sync(0xFFFFFFFF,passHit,(i|7)-8);
+                passHit=((i&16)?0:cumHit); // (i&16) receive
+                cumHit+=__shfl_sync(0xFFFFFFFF,passHit,(i|15)-16);
 
-            if (hit) {
-              blockPartner.jBlock=j+(i&31);
-              blockPartner.shift=shift;
-              blockPartner.exclAddress=-1; // No exclusions yet
-              // Use i/32 instead of iblock so it's at start of array.
-              blockPartners[maxPartnersPerBlock*(i/32)+partnerPos+cumHit-1]=blockPartner;
-            }
+                if (hit) {
+                  blockPartner.jBlock=j+(i&31);
+                  blockPartner.shift=shift;
+                  blockPartner.exclAddress=-1; // No exclusions yet
+                  // Use i/32 instead of iblock so it's at start of array.
+                  blockPartners[maxPartnersPerBlock*(i/32)+partnerPos+cumHit-1]=blockPartner;
+                }
 
-            // Update partner pos
-            __syncwarp();
-            partnerPos+=__shfl_sync(0xFFFFFFFF,cumHit,31);
+                // Update partner pos
+                __syncwarp();
+                partnerPos+=__shfl_sync(0xFFFFFFFF,cumHit,31);
           }
 
-          startBlock=0;
-        }
+        startBlock=0;
       }
     }
+  }
 
     if ((i&31)==0) {
       // use i/32 instead of iblock so it's at the start of the array
