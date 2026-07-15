db908685d923b45e6d4f9aba3a8a290a466e18ee
Ryan Lee Hayes <rhayes1@hpc3-gpu-l54-09.local>
2026-07-15 14:52:48 -0700

    Modernized blade src/CMakeLists.txt so BLaDE respects cuda architecture for optimal performance.

--- Compile.sh
+++ Compile.sh
@@ -9,9 +9,9 @@ cd build
 # -DRX=ON # Compile with replica exchange
 # -DUNITS=AKMA # Compile with AKMA units (GROMACS is the alternative)
 # -DPROFILE=ON # Compile with serial execution of all kernels for simpler profiling
-# -DCMAKE_CUDA_ARCHITECTURES=70 # Should compile for sm_70 devices, but it's broken
+# -DCMAKE_CUDA_ARCHITECTURES=70 # choose appropriately for your cuda hardware and compilers, but don't use below 60 unless you're willing to take a performance hit. Cuda 13 requires at least 75
 # -DCMAKE_BUILD_TYPE=Release || -DCMAKE_BUILD_TYPE=Debug
-cmake -DUNITS=AKMA -DCMAKE_BUILD_TYPE=Release ../src
+cmake -DCMAKE_CUDA_ARCHITECTURES=70 -DUNITS=AKMA -DCMAKE_BUILD_TYPE=Release ../src
 
 # -j8 # compile with 8 threads
 # VERBOSE=1 # Show all the compilation commands
